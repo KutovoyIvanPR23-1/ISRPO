e2bc8d3f7d83383f85b06296218f5d51b7dc19ed
KutovoyIvanPR23-1 <cubastik215@gmail.com>
2026-03-11 23:14:47 +0300

    tr

---
+++ PR8/PR8/PR8/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace PR8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxFrom = new System.Windows.Forms.ComboBox();
+            this.comboBoxTo = new System.Windows.Forms.ComboBox();
+            this.textBoxAmount = new System.Windows.Forms.TextBox();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.labelRates = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxFrom
+            // 
+            this.comboBoxFrom.FormattingEnabled = true;
+            this.comboBoxFrom.Location = new System.Drawing.Point(154, 85);
+            this.comboBoxFrom.Name = "comboBoxFrom";
+            this.comboBoxFrom.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxFrom.TabIndex = 0;
+            // 
+            // comboBoxTo
+            // 
+            this.comboBoxTo.FormattingEnabled = true;
+            this.comboBoxTo.Location = new System.Drawing.Point(154, 130);
+            this.comboBoxTo.Name = "comboBoxTo";
+            this.comboBoxTo.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxTo.TabIndex = 1;
+            // 
+            // textBoxAmount
+            // 
+            this.textBoxAmount.Location = new System.Drawing.Point(154, 205);
+            this.textBoxAmount.Name = "textBoxAmount";
+            this.textBoxAmount.Size = new System.Drawing.Size(121, 20);
+            this.textBoxAmount.TabIndex = 2;
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonConvert.Location = new System.Drawing.Point(281, 104);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(38, 32);
+            this.buttonConvert.TabIndex = 3;
+            this.buttonConvert.Text = "↔";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonSwap_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(151, 266);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(35, 13);
+            this.labelResult.TabIndex = 4;
+            this.labelResult.Text = "label1";
+            // 
+            // labelRates
+            // 
+            this.labelRates.AutoSize = true;
+            this.labelRates.Location = new System.Drawing.Point(26, 303);
+            this.labelRates.Name = "labelRates";
+            this.labelRates.Size = new System.Drawing.Size(35, 13);
+            this.labelRates.TabIndex = 5;
+            this.labelRates.Text = "label1";
+            this.labelRates.Click += new System.EventHandler(this.labelRates_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Конвертер валют";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(26, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Из:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(26, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "В:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(26, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Сумма:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(26, 263);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Результат:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 382);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelRates);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.textBoxAmount);
+            this.Controls.Add(this.comboBoxTo);
+            this.Controls.Add(this.comboBoxFrom);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxFrom;
+        private System.Windows.Forms.ComboBox comboBoxTo;
+        private System.Windows.Forms.TextBox textBoxAmount;
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Label labelRates;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ PR8/PR8/PR8/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR8
+{
+    public partial class Form1 : Form
+    {
+        // Курсы валют относительно RUB (рубля)
+        private readonly Dictionary<string, double> exchangeRates = new Dictionary<string, double>
+        {
+            { "RUB", 1.0 },      // Российский рубль
+            { "USD", 90.0 },     // Доллар США (примерный курс)
+            { "EUR", 98.0 },     // Евро (примерный курс)
+            { "CNY", 12.5 },     // Китайский юань (примерный курс)
+            { "KRW", 0.065 }     // Южнокорейская вона (примерный курс)
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCurrencyComboBoxes();
+            SetupEventHandlers();
+            
+        }
+
+        // Инициализация выпадающих списков валют
+        private void InitializeCurrencyComboBoxes()
+        {
+            // Добавляем валюты в оба списка
+            foreach (var currency in exchangeRates.Keys)
+            {
+                comboBoxFrom.Items.Add(currency);
+                comboBoxTo.Items.Add(currency);
+            }
+
+            // Устанавливаем значения по умолчанию
+            comboBoxFrom.SelectedItem = "USD";
+            comboBoxTo.SelectedItem = "RUB";
+            UpdateExchangeRatesInfo();
+        }
+
+        // Настройка обработчиков событий
+        private void SetupEventHandlers()
+        {
+            // Автоматическая конвертация при изменении данных
+            textBoxAmount.TextChanged += (s, e) => ConvertCurrency();
+            comboBoxFrom.SelectedIndexChanged += (s, e) => ConvertCurrency();
+            comboBoxTo.SelectedIndexChanged += (s, e) => ConvertCurrency();
+
+            // Если используете кнопку:
+            // buttonConvert.Click += (s, e) => ConvertCurrency();
+        }
+
+        // Основная функция конвертации
+        private void ConvertCurrency()
+        {
+            // Проверяем, что все поля заполнены
+            if (comboBoxFrom.SelectedItem == null ||
+                comboBoxTo.SelectedItem == null ||
+                string.IsNullOrWhiteSpace(textBoxAmount.Text))
+            {
+                labelResult.Text = "Введите данные";
+                return;
+            }
+
+            // Пытаемся получить числовое значение суммы
+            if (!double.TryParse(textBoxAmount.Text, out double amount))
+            {
+                labelResult.Text = "Некорректная сумма";
+                return;
+            }
+
+            string fromCurrency = comboBoxFrom.SelectedItem.ToString();
+            string toCurrency = comboBoxTo.SelectedItem.ToString();
+
+            // Выполняем конвертацию
+            double result = Convert(amount, fromCurrency, toCurrency);
+
+            // Отображаем результат с округлением до 2 знаков
+            labelResult.Text = $"{result:F2} {toCurrency}";
+        }
+
+        // Метод для расчета конвертации
+        private double Convert(double amount, string fromCurrency, string toCurrency)
+        {
+            // Конвертируем через RUB
+            double amountInRubles = amount * exchangeRates[fromCurrency];
+            double result = amountInRubles / exchangeRates[toCurrency];
+
+            return result;
+        }
+
+        // Обработчик для обмена валют местами
+        private void buttonSwap_Click(object sender, EventArgs e)
+        {
+            if (comboBoxFrom.SelectedItem != null && comboBoxTo.SelectedItem != null)
+            {
+                var temp = comboBoxFrom.SelectedItem;
+                comboBoxFrom.SelectedItem = comboBoxTo.SelectedItem;
+                comboBoxTo.SelectedItem = temp;
+                ConvertCurrency();
+            }
+        }
+        private void UpdateExchangeRatesInfo()
+        {
+            labelRates.Text = "Курсы к RUB:\n";
+            foreach (var rate in exchangeRates)
+            {
+                if (rate.Key != "RUB")
+                {
+                    labelRates.Text += $"1 {rate.Key} = {rate.Value} RUB\n";
+                }
+            }
+        }
+
+        private void labelRates_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
